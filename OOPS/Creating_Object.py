91679c7fe44644423b30506855dae379859ae738
Ashwani Yadav <ashwani811511@gmail.com>
2026-05-31 15:11:24 +0530

    Object Concept

---
+++ OOPS/Creating_Object.py
@@ -0,0 +1,13 @@
+class Ashwani:
+    course="Btech"
+   
+print(Ashwani.course)
+
+Ashwani.Rollno=15991
+print(Ashwani.Rollno)
+
+
+#Creating Object    
+run=Ashwani()
+print(run.course)
+print(run.Rollno)
